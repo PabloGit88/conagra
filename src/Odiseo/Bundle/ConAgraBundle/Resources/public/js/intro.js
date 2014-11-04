0c2c13656ca89bcbd060ceb7fe021172c05dbae2
AsusK55A <AsusK55A@Pablo-Note>
2014-11-04 18:36:48 -0300

    register and login form modificactions

---
+++ src/Odiseo/Bundle/ConAgraBundle/Resources/public/js/intro.js
@@ -0,0 +1,38 @@
+(function ( $ ) {
+    'use strict';
+
+    $(document).ready(function() 
+    {
+    	var sending = false;
+    	$(".registerForm form, .loginForm form").validate(
+    	{
+			onkeyup: false,
+			onclick: false,
+			onfocusout: false,
+			errorPlacement: function(error, element) 
+			{
+			},
+			highlight: function(element, errorClass, validClass) 
+			{
+			    $(element).addClass(errorClass).removeClass(validClass);
+			},
+			unhighlight: function(element, errorClass, validClass) 
+			{
+			    $(element).removeClass(errorClass).addClass(validClass);
+			},
+			invalidHandler: function(event, validator)
+			{
+				//alert("Debes completar todos los campos correctamente para continuar.");
+			},
+			submitHandler: function(form)
+			{	
+				if(sending == false)
+	        	{
+					sending = true;
+	        	}
+			}
+    			
+    	});
+     });
+
+})( jQuery );
