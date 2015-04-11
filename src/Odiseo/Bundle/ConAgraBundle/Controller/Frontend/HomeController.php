ce61e085f933b23bf49571ab625fecbe19142a75
Diego <songecko@gmail.com>
2015-04-11 02:57:50 -0300

    nuevo layout + inicio capitulo 1

--- src/Odiseo/Bundle/ConAgraBundle/Controller/Frontend/HomeController.php
+++ src/Odiseo/Bundle/ConAgraBundle/Controller/Frontend/HomeController.php
@@ -3,9 +3,15 @@
 namespace Odiseo\Bundle\ConAgraBundle\Controller\Frontend;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends Controller
 {
+	public function welcomeAction(Request $request)
+	{
+		return $this->render('OdiseoConAgraBundle:Frontend:welcome.html.twig');
+	}
+	
     public function showHomeAction( )
     {
         return $this->render('OdiseoConAgraBundle:Frontend:home.html.twig');
